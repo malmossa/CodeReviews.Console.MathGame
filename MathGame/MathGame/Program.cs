32ebec68e8a32923fbefabc5629ced9af686484a
Mansor Almossa <malmossa@gmail.com>
2024-08-20 10:09:01 -0700

    Project completed

--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -4,7 +4,6 @@ Console.Title = "Math Game";
 Console.ForegroundColor = ConsoleColor.White;
 
 UI display = new UI();
-List<string> games = new List<string>();
 
 display.Header();
 string name = Helpers.GetName();
